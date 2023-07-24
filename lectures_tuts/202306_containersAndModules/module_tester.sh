be516be07f70bed3cceffc8491df0b3e5923b837
JonoLF <jono.faller@gmail.com>
2023-07-24 17:41:43 +0200

    edit to module tester

--- lectures_tuts/202306_containersAndModules/module_tester.sh
+++ lectures_tuts/202306_containersAndModules/module_tester.sh
@@ -1,14 +1,14 @@
 #!/bin/env bash
 
 # Check if OpenMPI is in the environment
-if command -v mpicc.openmpi >/dev/null 2>&1; then
+if command -v mpirun.openmpi >/dev/null 2>&1; then
     # Get the OpenMPI version
-    openmpi_version=$(mpicc.openmpi --showme:version | awk '{print $NF}')
+    openmpi_version=$(mpirun.openmpi --version | awk '/mpirun/ {print $NF}')
     echo "OpenMPI - $openmpi_version"
 # Check if MPICH is in the environment
-elif command -v mpicc.mpich >/dev/null 2>&1; then
+elif command -v mpirun.mpich >/dev/null 2>&1; then
     # Get the MPICH version
-    mpich_version=$(mpicc.mpich -v 2>&1 | awk '/version/ {print $NF}')
+    mpich_version=$(mpirun.mpich --version | awk '/mpirun/ {print $NF}')
     echo "MPICH - $mpich_version"
 else
     echo "No MPI found in environment"
