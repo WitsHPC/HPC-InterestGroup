55f214a5f2b52bcded83f64d67940ba6c2b8e671
JonoLF <jono.faller@gmail.com>
2023-07-27 14:37:54 +0200

    final fix

--- team_selection/202306_containersAndModules/module_tester.sh
+++ team_selection/202306_containersAndModules/module_tester.sh
@@ -2,10 +2,10 @@
 
 # Check if OpenMPI is in the environment
 if mpirun --version |& grep -q "Open MPI"; then
-    mpi_info=$(mpirun --version | awk '/mpirun/ {print "OpenMPI - " $4}')
+    mpi_info=$(mpirun --version | awk '/mpirun/ {print "OpenMPI - " $NF}')
     echo "$mpi_info"
 elif mpirun --version |& grep -q "MPICH"; then
-    mpi_info=$(mpirun --version | awk '/mpirun/ {print "MPICH - " $4}')
+    mpi_info=$(mpirun --version | awk '/mpirun/ {print "MPICH - " $NF}')
     echo "$mpi_info"
 else
     echo "No MPI found in environment"
