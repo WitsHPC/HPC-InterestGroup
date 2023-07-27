9dd2c2532033f1c50dcfec426976117b5df7b00b
JonoLF <jono.faller@gmail.com>
2023-07-27 14:35:42 +0200

    .

--- team_selection/202306_containersAndModules/module_tester.sh
+++ team_selection/202306_containersAndModules/module_tester.sh
@@ -1,9 +1,11 @@
 #!/bin/env bash
 
 # Check if OpenMPI is in the environment
-if command -v mpirun >/dev/null 2>&1; then
-    # Get the MPI type and version
-    mpi_info=$(mpirun --version | awk '/mpirun/ {if ($3 == "(Open MPI)") print "OpenMPI - " $4; else if ($3 == "(MPICH)") print "MPICH - " $4}')
+if mpirun --version |& grep -q "Open MPI"; then
+    mpi_info=$(mpirun --version | awk '/mpirun/ {print "OpenMPI - " $4}')
+    echo "$mpi_info"
+elif mpirun --version |& grep -q "MPICH"; then
+    mpi_info=$(mpirun --version | awk '/mpirun/ {print "MPICH - " $4}')
     echo "$mpi_info"
 else
     echo "No MPI found in environment"
